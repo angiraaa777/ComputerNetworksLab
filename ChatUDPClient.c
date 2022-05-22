05dce59f2d101cb89ffacea686c12adb7892e873
angiraaa777 <64687738+angiraaa777@users.noreply.github.com>
2022-05-22 17:47:43 +0530

    Add files via upload

---
+++ ChatUDPClient.c
@@ -0,0 +1,90 @@
+
+/*UDP Echo CLIENT Program*/
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define n 256
+
+struct sockaddr_in serv_addr, cli_addr;
+int skfd, r, w;
+
+unsigned short serv_port = 25021; /*port number to be used by the CLIENT*/
+char serv_ip[] = "127.0.0.1";     /*CLIENT's IP-address*/
+
+int length = sizeof(struct sockaddr_in);
+char buffer[n]; /*buffer for sending and receiving messages*/
+
+int main(int argc, char *argv[])
+{
+    if (argc > 1)
+    {
+        serv_port = atoi(argv[1]);
+        printf("SERVER PORT no. : %d \n ", serv_port);
+    }
+
+    if (argc > 2)
+    {
+        strcpy(serv_ip, argv[2]);
+        printf("SERVER ip address: %s \n", serv_ip);
+    }
+
+    bzero(&serv_addr, sizeof(serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(serv_port);
+    inet_aton(serv_ip, (&serv_addr.sin_addr));
+
+    printf("Live Chat Client\n");
+
+    if ((skfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        printf("CLIENT ERROR: Can't create socket");
+        exit(1);
+    }
+
+    printf("CLIENT: Connected to the Server\n");
+
+    while (1)
+    {
+        printf("CLIENT Turn: \n");
+        bzero(buffer, n);
+        fgets(buffer, n, stdin);
+
+        if (sendto(skfd, buffer, n, 0, (struct sockaddr *)&serv_addr, length) < 0)
+        {
+            printf("SERVER: Can't send messages to the live server.\n");
+            close(skfd);
+            exit(1);
+        }
+
+        if (strncmp("Bye", buffer, 3) == 0)
+        {
+            break;
+        }
+
+        printf("Waiting for SERVER..... \n");
+        bzero(buffer, n);
+
+        if (recvfrom(skfd, buffer, n, 0, (struct sockaddr *)&serv_addr, &length) < 0)
+        {
+            printf("SERVER ERROR : Can't read.\n");
+            close(skfd);
+            exit(1);
+        }
+
+        printf("CLIENT: %s \n", buffer);
+        if (strncmp("Bye", buffer, 3) == 0)
+            break;
+    }
+
+    close(skfd);
+    return 0;
+}
---
+++ ChatUDPServer.c
@@ -0,0 +1,113 @@
+
+/*UDP Echo Server Program*/
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define n 256
+
+struct sockaddr_in serv_addr, cli_addr;
+int listenfd, connfd, r, w;
+socklen_t cli_addr_len;
+
+unsigned short serv_port = 25021; /*port number to be used by the server*/
+char serv_ip[] = "127.0.0.1";     /*server's IP-address*/
+
+char buffer[n]; /*buffer for sending and receiving messages*/
+int length=sizeof(struct sockaddr_in);
+
+int main(int argc, char *argv[])
+{
+    if (argc > 1)
+    {
+        serv_port = atoi(argv[1]);
+        printf("SERVER PORT no. : %d \n ", serv_port);
+    }
+
+    if (argc > 2)
+    {
+        strcpy(serv_ip, argv[2]);
+        printf("SERVER ip address: %s \n", serv_ip);
+    }
+
+    bzero(&serv_addr, sizeof(serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(serv_port);
+    inet_aton(serv_ip, (&serv_addr.sin_addr));
+
+    printf("Live Chat Server");
+
+    if ((listenfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        printf("SERVER ERROR: Can't create socket");
+        exit(1);
+    }
+
+    if (bind(listenfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("SERVER ERROR: Can't bind\n");
+            close(listenfd);
+        exit(1);
+    }
+
+    int maxCli = 5;
+
+    cli_addr_len = sizeof(cli_addr);
+
+    printf("______________________________________________________________________\n");
+
+    for (int client = 1; client <= maxCli; client++)
+    {
+        printf("________________________________________________\n");
+
+        printf("SERVER: Listening for Client %d....\n", client);
+        printf("\nSERVER: Connection from client accepted.\n");
+    }
+
+    while (1)
+    {
+        printf("Waiting for Client....\n");
+        bzero(buffer, n);
+
+        if (recvfrom(listenfd, buffer, n, 0, (struct sockaddr *)&serv_addr, &length) < 0)
+        {
+            printf("SERVER ERROR: Can't read\n");
+            close(listenfd);
+            close(connfd);
+            exit(1);
+        }
+
+        printf("CLIENT : %s \n", buffer);
+
+        if (strncmp("Bye", buffer, 3) == 0)
+        {
+            break;
+        }
+
+        printf("SERVER Turn: \n");
+        bzero(buffer, n);
+
+        fgets(buffer, n, stdin);
+
+        if (sendto(listenfd, buffer, n, 0, (struct sockaddr *)&serv_addr, length) < 0)
+        {
+            printf("SERVER : Can't send message to live chat client.\n");
+            close(listenfd);
+            close(connfd);
+            exit(1);
+        }
+
+        if (strncmp("Bye", buffer, 3) == 0)
+            break;
+    }
+    close(connfd);
+    printf("______________________________________________________\n");
+}
